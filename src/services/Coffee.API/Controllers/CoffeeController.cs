a3141ac8a4f45e0b8f5d7b228be3dc4c472daea6
Zohaib Khan <mr.zohaib@live.com>
2023-09-16 04:27:20 +0500

    Using Envoy as a gatway for web apis

---
+++ src/services/Coffee.API/Controllers/CoffeeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        private static readonly string[] Coffees = new[]
+        {
+        "Nutty"
+        ,"Chocolatey"
+        ,"Fruity"
+        ,"Floral"
+        ,"Spicy"
+        ,"Earthy"
+        ,"Caramel"
+        ,"Smoky"
+        ,"Nutritional"
+        ,"Herbal"
+    };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rnd = new Random();
+
+            return Ok(Coffees[rnd.Next(Coffees.Length)]);
+        }
+    }
+}
---
+++ src/services/Tea.API/Controllers/TeaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tea.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeaController : ControllerBase
+    {
+        private static readonly string[] Teas = new[]
+        {
+        "Nutty"
+        ,"Chocolatey"
+        ,"Fruity"
+        ,"Floral"
+        ,"Spicy"
+        ,"Earthy"
+        ,"Caramel"
+        ,"Smoky"
+        ,"Nutritional"
+        ,"Herbal"
+    };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rnd = new Random();
+
+            return Ok(Teas[rnd.Next(Teas.Length)]);
+        }
+    }
+}
